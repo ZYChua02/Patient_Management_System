9346253773b3e084d181f2cebaf0c6a96771ad66
Zhe Yu Chua <s10196605@connect.np.edu.sg>
2020-02-05 01:20:14 +0800

    Tried to do the webapi

---
+++ Pm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRG2_T08_Team2
+{
+    public class RootObject
+    {
+        public List<RegionMetadata> region_metadata { get; set; }
+        public List<Item> items { get; set; }
+        public ApiInfo api_info { get; set; }
+    }
+    public class LabelLocation
+    {
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+    }
+
+    public class RegionMetadata
+    {
+        public string name { get; set; }
+        public LabelLocation label_location { get; set; }
+    }
+
+    public class Pm25OneHourly
+    {
+        public int west { get; set; }
+        public int east { get; set; }
+        public int central { get; set; }
+        public int south { get; set; }
+        public int north { get; set; }
+    }
+
+    public class Readings
+    {
+        public Pm25OneHourly pm25_one_hourly { get; set; }
+    }
+
+    public class Item
+    {
+        public DateTime timestamp { get; set; }
+        public DateTime update_timestamp { get; set; }
+        public Readings readings { get; set; }
+    }
+
+    public class ApiInfo
+    {
+        public string status { get; set; }
+    }
+
+    
+}

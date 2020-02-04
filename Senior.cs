048e7f47bae49cbfd60deb6c9b554be23b9db36f
Zhe Yu Chua <s10196605@connect.np.edu.sg>
2020-02-04 19:57:10 +0800

    Figuring out Calculating charges

--- Senior.cs
+++ Senior.cs
@@ -20,9 +20,14 @@ namespace PRG2_T08_Team2
         public override double CalculateCharges()
         {
             //NOT DONE: Need to associate with bedStayList
-            foreach (BedStay bes in p.Stay.)
-            //double charges = 0.0;
+            foreach (BedStay bes in Stay.BedStayList)
+            {
+                double charges = total * 0.5;
+                
+            }
             return charges;
+
+
         }
 
         public override string ToString()

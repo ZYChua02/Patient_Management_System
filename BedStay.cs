d0a83726a72dc28241813a566529f8fc89ebdf23
Chin Tiong Ryan Ng <s10196904@connect.np.edu.sg>
2020-02-05 06:56:35 +0800

    Fixed nullable type

--- BedStay.cs
+++ BedStay.cs
@@ -14,7 +14,7 @@ namespace PRG2_T08_Team2
     {
         public DateTime StartBedStay { get; set; }
 
-        public DateTime EndBedStay { get; set; }
+        public DateTime? EndBedStay { get; set; }
 
         public Bed Bed { get; set; }
         
